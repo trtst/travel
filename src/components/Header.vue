51a8ae6429ec575fc505679fe411bae45c374f71
yangpenghui <yangpenghui@moer.cn>
2019-07-04 18:06:14 +0800

    项目初始化

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="header">
+        <div class="header-l">0</div>
+        <div class="header-main">
+            <input type="text" placeholder="输入内容">
+        </div>
+        <div class="header-r">2</div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style lang="less" scoped>
+    .header {
+        display: flex;
+        height: .86rem;
+        font-size: 0;
+        color: #fff;
+        background: #00bcd4;
+        .header-l {
+            display: inline-block;
+            width: .64rem;
+        }
+        .header-main {
+            display: inline-block;
+            flex: 1;
+            padding: .1rem 0;
+        }
+        .header-r {
+            display: inline-block;
+            width: 1.24rem;
+        }
+    }
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <i-header></i-header>
+    </div>
+</template>
+
+<script>
+    import iHeader from '@/components/Header'
+    export default {
+        name: 'index',
+        components: {
+            iHeader
+        }
+    }
+</script>
+
+<style>
+</style>
